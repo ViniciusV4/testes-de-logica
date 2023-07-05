a1b300b70e8edb572e8afbf91c911ad8e2d0a440
Marcos Vinicius <viniciusmouraa234@gmail.com>
2023-07-05 11:31:26 -0300

    second text update

--- 2-Olá/main.js
+++ 2-Olá/main.js
@@ -3,6 +3,6 @@ function sent() {
     let nameValue = namePerson.value
     let textBox = document.getElementById("space")
 
-    textBox.textContent = `Hello ${nameValue}, it's nice to meet you`
+    textBox.textContent = `Olá ${nameValue}, é um prazer te conhecer!`
 }
 
