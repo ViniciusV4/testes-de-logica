48e20a105481b48196627034b78c009080245a5b
Marcos Vinicius <viniciusmouraa234@gmail.com>
2023-07-07 12:24:58 -0300

    excluindo codigo incompleto

--- 5-Contando com for/contando.js
+++
@@ -1,9 +0,0 @@
-alert("Olá, bem-vindo ao contador")
-let valorUm = prompt("Digite um valor inicial")
-let valorDois = prompt("Digite um valor final")
-let espacamento = prompt("Digite de quanto em quanto você deseja pular")
-let soma = ""
-
-for(let c = valorUm; c <= valorDois; c = c + espacamento) {
-    console.log(`contando: ${c}`)
-}
